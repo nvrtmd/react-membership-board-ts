59a68206eb726e49a25ff6e77b65f42948d147e0
YUZAMIN <untmd.crmn@gmail.com>
2023-01-18 01:45:00 +0900

    design: theme 파일 생성
    - 필요 시 프로젝트 전역에서 사용 가능한 theme 파일 생성
    - color, layout 속성 포함
    Fixes: #6

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const color = {
+  primary: '',
+  secondary: '',
+  tertiary: '',
+  white: '#fff',
+  black: '#0b0b0b',
+};
+
+const layout = {
+  bottomNavBarHeight: '2.8rem',
+};
+
+export const theme = {
+  color,
+  layout,
+};
