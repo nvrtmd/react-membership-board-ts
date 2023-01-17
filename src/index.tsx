c1ee6d5b60a85574a432d6fc7c03486098e0de8a
YUZAMIN <untmd.crmn@gmail.com>
2023-01-18 01:51:32 +0900

    feat: tsconfig.json 파일에 baseUrl 속성 추가
    - 모듈 import 시 절대 경로를 사용할 수 있게 tsconfig 파일에 baseUrl을
      src로 지정
    Ref: #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyle from './styles/GlobalStyle';
+import App from 'App';
+import GlobalStyle from 'styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
