2bbb0f48bdf83a04b7adb1426dbe972ce580ad3a
YUZAMIN <untmd.crmn@gmail.com>
2023-01-19 02:41:20 +0900

    feat: StartButton 외의 영역 클릭 시 isClicked false로 변경
    - StartButton이 클릭된 상태에서 버튼 외 영역 클릭 시 클릭되지 않은
      상태로 변경하는 함수 생성
    - 해당 함수를 document의 mousedown 이벤트 핸들러로 등록
    Fixes: #8

--- src/components/layouts/BottomNavBar.tsx
+++ src/components/layouts/BottomNavBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import styled from 'styled-components/macro';
 import { theme } from 'styles/theme';
 import StartImg from 'assets/start_img.png';
@@ -6,15 +6,30 @@ import { Button } from 'components/common/Button';
 
 export const BottomNavBar = () => {
   const [isClicked, setIsClicked] = useState<boolean>(false);
+  const startButtonRef = useRef<HTMLDivElement>(null);
 
   const handleStartButtonClick = useCallback(() => {
     setIsClicked((prev) => !prev);
   }, []);
 
+  const handleNonMenuAreaClick = (e: Event) => {
+    const target = e.target as HTMLDivElement;
+    if (isClicked && startButtonRef.current && !startButtonRef.current.contains(target)) {
+      setIsClicked(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', (e: Event) => handleNonMenuAreaClick(e));
+    return () => {
+      document.removeEventListener('mousedown', handleNonMenuAreaClick);
+    };
+  }, [isClicked]);
+
   return (
     <>
       <Wrapper>
-        <Button isClicked={isClicked} name="Start" clickHandler={handleStartButtonClick}>
+        <Button isClicked={isClicked} name="Start" buttonRef={startButtonRef} clickHandler={handleStartButtonClick}>
           <StartImage src={StartImg} alt="start_image" />
         </Button>
       </Wrapper>
