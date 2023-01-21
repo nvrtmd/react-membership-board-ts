73f80426e66d693debb7edfe766fa832d1aecae8
YUZAMIN <untmd.crmn@gmail.com>
2023-01-21 21:15:12 +0900

    feat: Browser 컴포넌트 생성
    - Browser 컴포넌트 생성
    - 컴포넌트 내에 필요한 이미지 assets 폴더 내에 추가
    Fixes: #10

---
+++ src/components/common/Browser.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components/macro';
+
+export const Browser = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled.div`
+  background: #c0c0c0;
+  padding: 5px;
+  width: 90%;
+  height: 90%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 1.5px 1.5px 0px 0px #dfdfdf inset, -1.5px -1.5px 0px 0px #020215 inset;
+  -webkit-box-shadow: 1.5px 1.5px 0px 0px #dfdfdf inset, -1.5px -1.5px 0px 0px #020215 inset;
+  -moz-box-shadow: 1.5px 1.5px 0px 0px #dfdfdf inset, -1.5px -1.5px 0px 0px #020215 inset;
+`;
