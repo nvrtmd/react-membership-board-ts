c0ed8c666320271cda9d2f81a7a3b793b47749f0
YUZAMIN <untmd.crmn@gmail.com>
2023-01-26 00:40:22 +0900

    feat: API 통신을 통해 post list를 fetch
    - axios를 사용하여 post list를 fetch
      - async/await를 사용한 비동기 처리
    - fetched data를 ppostList state에 할당
      - 게시글의 type을 Post interface로 미리 지정하여 사용
    Fixes: #13

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
 import { Browser } from 'components/common/Browser';
 import { Layout } from 'components/layouts/Layout';
+import axios from 'axios';
+
+interface Post {
+  post_idx: number;
+  post_title: string;
+  post_contents: string;
+  createdAt: Date;
+  updatedAt: Date;
+  member_idx: number;
+  post_writer: {
+    member_id: string;
+    member_nickname: string;
+  };
+}
 
 const BoardPage = () => {
+  const [postList, setPostList] = useState<Post[]>();
+
+  const fetchPostList = async () => {
+    const fetchedData = await axios.get('/post/list');
+    setPostList(fetchedData.data.data);
+  };
+
+  useEffect(() => {
+    fetchPostList();
+  }, []);
+
   return (
     <Layout>
       <Browser route="board" />
