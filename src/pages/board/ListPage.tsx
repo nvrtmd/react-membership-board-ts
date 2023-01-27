fcdc57548cba648790c71652b58e78af3130b953
YUZAMIN <untmd.crmn@gmail.com>
2023-01-27 18:17:28 +0900

    design: 게시글 목록과 게시글의 Wrapper 컴포넌트 생성
    Fixes: #15

--- src/pages/board/ListPage.tsx
+++ src/pages/board/ListPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components/macro';
 import { Browser } from 'components/common/Browser';
 import { Layout } from 'components/layouts/Layout';
 import axios from 'axios';
@@ -19,8 +20,26 @@ export const ListPage = () => {
   return (
     <Layout>
       <Browser route="board/list">
-        {postList && postList.map((post) => <div key={post.post_idx}>{post.post_contents}</div>)}
+        <ListWrapper>
+          {postList &&
+            postList.map((post) => (
+              <PostWrapper key={post.post_idx}>
+                {post.post_contents} {post.post_writer.member_nickname}
+              </PostWrapper>
+            ))}
+        </ListWrapper>
       </Browser>
     </Layout>
   );
 };
+
+const ListWrapper = styled.div`
+  padding: 0.5rem 2.5rem;
+`;
+
+const PostWrapper = styled.div`
+  border: 1px solid green;
+  margin: 2rem 0;
+  padding: 2rem;
+  box-shadow: -0.4rem 0 0 0 black, 0.4rem 0 0 0 black, 0 -0.4rem 0 0 black, 0 0.4rem 0 0 black;
+`;
