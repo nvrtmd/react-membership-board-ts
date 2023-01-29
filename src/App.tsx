db48673a1970a1a6641ea5a132a01497bd6e5f3f
YUZAMIN <untmd.crmn@gmail.com>
2023-01-30 02:04:37 +0900

    feat: SignupPage 페이지 컴포넌트 생성
    - BottomNavBar의 Menu의 Sign up 버튼을 클릭 시 /auth/signup 페이지로
      이동
    Fixes: #23

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import HomePage from 'pages/HomePage';
 import { ListPage as PostListPage } from 'pages/board/ListPage';
 import { PostPage } from 'pages/board/PostPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SignupPage } from 'pages/auth/SignupPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/board/list" element={<PostListPage />} />
         <Route path="/board/:postIdx" element={<PostPage />} />
+        <Route path="/auth/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/auth/SignupPage.tsx
@@ -0,0 +1,5 @@
+import { Layout } from 'components/layouts/Layout';
+
+export const SignupPage = () => {
+  return <Layout>Hello, This is Signup page!</Layout>;
+};
