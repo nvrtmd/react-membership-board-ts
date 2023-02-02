3386b69f3cc025159ca7969fd65273e31917d8a6
YUZAMIN <untmd.crmn@gmail.com>
2023-02-03 00:45:51 +0900

    feat: 로그인 기능을 수행하는 signIn 메소드 생성
    - auth 객체 내에 signIn 메서드 구현
    - SignInPage의 Input 컴포넌트를 통해 입력받은 회원 정보를 body에 넣어
      request
    Fixes: #27

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -16,6 +16,20 @@ const auth = {
       }
     }
   },
+  signIn: async (data: Member) => {
+    try {
+      await axios.post(`/auth/signin`, data);
+    } catch (err) {
+      if (isAxiosError(err) && err.response) {
+        // TODO: 추후 백엔드에서 에러 세분화하여 전달하면 적절한 에러 처리 필요
+        // status code에 따라 각각 대응 필요
+        alert('존재하지 않는 아이디 또는 비밀번호입니다.');
+        throw {
+          error: '존재하지 않는 아이디 또는 비밀번호입니다.',
+        };
+      }
+    }
+  },
 };
 
 export { auth };
