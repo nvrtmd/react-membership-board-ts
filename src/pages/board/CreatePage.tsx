454e1d4bd9ba2bcaff833cba3125d8b832c4c6f2
YUZAMIN <untmd.crmn@gmail.com>
2023-02-03 19:13:08 +0900

    feat: PostForm 컴포넌트 구현
    - Input, TextArea 컴포넌트로 이루어진 PostForm 컴포넌트 구현
    - useInput custom hook을 사용하여 입력값 제어
    - Create 버튼 컴포넌트 생성
    Resolves: #29

--- src/pages/board/CreatePage.tsx
+++ src/pages/board/CreatePage.tsx
@@ -1,13 +1,49 @@
 import styled from 'styled-components/macro';
-import { theme } from 'styles/theme';
 import { Browser } from 'components/common/Browser';
 import { Layout } from 'components/layouts/Layout';
+import { Input } from 'components/common/Input';
+import { Button } from 'components/common/Button';
+import { TextArea } from 'components/common/TextArea';
+import { useInput } from 'hooks/useInput';
 
 export const CreatePage = () => {
+  const { inputValue: title, handleInputChange: handleTitleChange } = useInput();
+  const { inputValue: contents, handleInputChange: handleContentsChange } = useInput();
+
+  const handleSignUpFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(title, contents);
+  };
+
   return (
     <Layout>
       <BrowserWrapper>
-        <Browser>This is create page</Browser>
+        <Browser>
+          <PostFormWrapper>
+            <PostForm onSubmit={handleSignUpFormSubmit}>
+              <PageTitle>- Create Post -</PageTitle>
+              <Input
+                name="title"
+                type="title"
+                value={title}
+                changeHandler={handleTitleChange}
+                placeholder="Write your title"
+              />
+              <ContentsWrapper>
+                <TextArea
+                  placeholder="Write your contents"
+                  name="comment"
+                  changeHandler={handleContentsChange}
+                  value={contents}
+                />
+              </ContentsWrapper>
+              <ButtonWrapper>
+                <Button name="Create" type="submit" />
+              </ButtonWrapper>
+            </PostForm>
+          </PostFormWrapper>
+        </Browser>
       </BrowserWrapper>
     </Layout>
   );
@@ -16,4 +52,30 @@ export const CreatePage = () => {
 const BrowserWrapper = styled.div`
   display: flex;
   height: 100%;
+`;
+const PostFormWrapper = styled.div`
+  min-height: 100%;
+  display: flex;
+`;
+
+const PostForm = styled.form`
+  width: 100%;
+  padding: 2rem 1.5rem;
+`;
+
+const PageTitle = styled.div`
+  text-align: center;
+  font-size: 1.6rem;
+  margin: 0.5rem 0 1rem;
+`;
+
+const ContentsWrapper = styled.div`
+  margin-top: 1.5rem;
+  height: 100%;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
 `;
