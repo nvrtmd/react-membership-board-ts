b510a75feab5ea02bc5a69fa545b73fada8aa05a
YUZAMIN <untmd.crmn@gmail.com>
2023-02-03 18:32:08 +0900

    refactor: api 요청 시 catch문의 조건문 제거
    - API 통신 중 에러 발생 시 조건 없이 바로 에러 객를 throw하도록 개선
    Fixes: #29

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,29 +6,21 @@ const auth = {
   signUp: async (data: Member) => {
     try {
       await axios.post(`/auth/signup`, data);
-    } catch (err) {
-      if (isAxiosError(err) && err.response) {
-        // TODO: 추후 백엔드에서 에러 세분화하여 전달하면 적절한 에러 처리 필요
-        // status code에 따라 각각 대응 필요
-        alert('중복된 아이디입니다.');
-        throw {
-          error: '중복된 아이디입니다.',
-        };
-      }
+    } catch {
+      alert('중복된 아이디입니다.');
+      throw {
+        error: '중복된 아이디입니다.',
+      };
     }
   },
   signIn: async (data: Member) => {
     try {
       await axios.post(`/auth/signin`, data, { withCredentials: true });
-    } catch (err) {
-      if (isAxiosError(err) && err.response) {
-        // TODO: 추후 백엔드에서 에러 세분화하여 전달하면 적절한 에러 처리 필요
-        // status code에 따라 각각 대응 필요
-        alert('존재하지 않는 아이디 또는 비밀번호입니다.');
-        throw {
-          error: '존재하지 않는 아이디 또는 비밀번호입니다.',
-        };
-      }
+    } catch {
+      alert('존재하지 않는 아이디 또는 비밀번호입니다.');
+      throw {
+        error: '존재하지 않는 아이디 또는 비밀번호입니다.',
+      };
     }
   },
   isSignedIn: async () => {
--- src/api/member.ts
+++ src/api/member.ts
@@ -1,17 +1,14 @@
-import axios, { isAxiosError } from 'axios';
-import { Member } from 'global/types';
+import axios from 'axios';
 
 const member = {
   getMemberInfo: async () => {
     try {
       const memberData = await axios.get(`/member/info`, { withCredentials: true });
       return memberData.data.data;
-    } catch (err) {
-      if (isAxiosError(err) && err.response) {
-        throw {
-          error: '서버로부터 회원 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
-        };
-      }
+    } catch {
+      throw {
+        error: '서버로부터 회원 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+      };
     }
   },
 };
