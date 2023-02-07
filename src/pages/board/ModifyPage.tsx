6515ce59db7f8018b98bee0065a54e187c8d96ce
YUZAMIN <untmd.crmn@gmail.com>
2023-02-07 19:10:14 +0900

    feat: ModifyPage에서 PostForm 컴포넌트 사용하여 게시글 수정 기능 구현
    - useParams를 사용하여 해당 게시글의 postIdx를 추출하여 기존 게시글 내용
      fetch
    - 기존 게시글 value를 PostForm에 value로 주입
    Fixes: #33

--- src/pages/board/ModifyPage.tsx
+++ src/pages/board/ModifyPage.tsx
@@ -1,12 +1,83 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { Browser } from 'components/common/Browser';
 import { Layout } from 'components/layouts/Layout';
+import { useInput } from 'hooks/useInput';
+import { board } from 'api/board';
+import { PostForm } from 'components/board/PostForm';
 
 export const ModifyPage = () => {
+  const { inputValue: title, setInputValue: setTitle, handleInputChange: handleTitleChange } = useInput();
+  const { inputValue: contents, setInputValue: setContents, handleInputChange: handleContentsChange } = useInput();
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchPostData();
+  }, []);
+
+  const fetchPostData = async () => {
+    try {
+      if (params.postIdx) {
+        const fetchedData = await board.getPostData(params.postIdx);
+        setTitle(fetchedData.post_title);
+        setContents(fetchedData.post_contents);
+      } else {
+        alert('게시글이 존재하지 않습니다.');
+        navigate(-1);
+      }
+    } catch {
+      alert('서버로부터 게시글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.');
+      navigate(-1);
+    }
+  };
+
+  const isPostFormInputValid = () => {
+    if (!title.length || !contents.length) {
+      alert('제목 또는 본문을 작성하세요.');
+      return false;
+    }
+    return true;
+  };
+
+  const handlePostFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isPostFormInputValid() && params.postIdx) {
+      try {
+        await board.modifyPost(params.postIdx, { title, contents });
+        alert('게시글이 수정되었습니다.');
+        navigate(-1);
+      } catch (err) {
+        alert('게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+      }
+    }
+  };
+
+  const handleCancelButtonClick = () => {
+    if (confirm('게시글 수정을 취소하시겠습니까?')) {
+      navigate(-1);
+    } else {
+      return;
+    }
+  };
+
   return (
     <Layout>
       <BrowserWrapper>
-        <Browser>Hello, This is modify page.</Browser>
+        <Browser>
+          <PostFormWrapper>
+            <PostForm
+              submitHandler={handlePostFormSubmit}
+              formTitle="- Modify Post -"
+              titleValue={title}
+              titleChangeHandler={handleTitleChange}
+              contentsValue={contents}
+              contentsChangeHandler={handleContentsChange}
+              cancelButtonClickHandler={handleCancelButtonClick}
+            />
+          </PostFormWrapper>
+        </Browser>
       </BrowserWrapper>
     </Layout>
   );
@@ -15,4 +86,9 @@ export const ModifyPage = () => {
 const BrowserWrapper = styled.div`
   display: flex;
   height: 100%;
+`;
+
+const PostFormWrapper = styled.div`
+  min-height: 100%;
+  display: flex;
 `;
