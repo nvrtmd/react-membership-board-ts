a3d7e09b98e611f7d67c243c3aeb77430c3eb050
YUZAMIN <untmd.crmn@gmail.com>
2023-02-09 01:52:09 +0900

    refactor: modifyPost 메소드 내 catch문 수정
    - CustomError 형식에 맞게 error throw
    - ModifyPage 내에서 error 객체 내 message를 사용하여 alert
    Fixes: #51

--- src/api/board.ts
+++ src/api/board.ts
@@ -54,7 +54,8 @@ const board = {
       await axios.patch(`/post/${postIdx}`, data, { withCredentials: true });
     } catch {
       throw {
-        error: '게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+        code: 500,
+        message: '게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.',
       };
     }
   },
