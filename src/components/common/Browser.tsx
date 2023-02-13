faeb52c2d369eae792d91eccf0e3fde8762637d5
YUZAMIN <untmd.crmn@gmail.com>
2023-02-14 02:57:55 +0900

    design: ListPage table 레이아웃 수정
    Fixes: #64

--- src/components/common/Browser.tsx
+++ src/components/common/Browser.tsx
@@ -90,7 +90,7 @@ const ControlButtons = () => {
 const Wrapper = styled.div`
   background: #c0c0c0;
   padding: 5px;
-  width: 90%;
+  width: 100%;
   max-width: 70rem;
   height: 90%;
   margin: auto;
