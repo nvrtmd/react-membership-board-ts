0e4503fe0f67573c5f9ea59ce95d73479216a350
YUZAMIN <untmd.crmn@gmail.com>
2023-02-18 17:31:12 +0900

    feat: SideNavBar 최적화
    - 컴포넌트를 memo로 wrapping하여 불필요한 리렌더링 방지
    Fixes: #78

--- src/components/layouts/SideNavBar.tsx
+++ src/components/layouts/SideNavBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, memo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { theme } from 'styles/theme';
@@ -20,7 +20,7 @@ interface NavItemContentProps {
   isClicked: boolean;
 }
 
-export const SideNavBar = () => {
+export const SideNavBar = memo(() => {
   const [clickedNavItem, setClickedNavItem] = useState('');
 
   const handleNavItemClick = useCallback((navItemName: string) => {
@@ -75,7 +75,7 @@ export const SideNavBar = () => {
       />
     </Wrapper>
   );
-};
+});
 
 const NavItem = ({ name, image, handleClick, isClicked, route }: NavItemProps) => {
   const navigate = useNavigate();
