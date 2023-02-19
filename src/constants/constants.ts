8e9a45617056e5f144fd0d2dbd856e39e8a28b3a
YUZAMIN <untmd.crmn@gmail.com>
2023-02-20 01:41:36 +0900

    feat: constants 내 상수 수정
    Fixes: #84

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -34,7 +34,6 @@ export const BOARD_ALERT_MESSAGE = {
   POST_NOT_EXIST_ALERT: '게시글이 존재하지 않습니다.',
   POST_DELETE_CONFIRM: '게시글을 삭제하시겠습니까?',
   POST_DELETED_ALERT: '게시글이 삭제되었습니다.',
-  CANNOT_GET_POST_INFO_FROM_SERVER_ALERT: '서버로부터 게시글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
   CONTENTS_EMPTY_ALERT: '내용을 작성하세요.',
   TITLE_OR_CONTENTS_EMPTY_ALERT: '제목 또는 본문을 작성하세요.',
   POST_MODIFIED_ALERT: '게시글이 수정되었습니다.',
@@ -49,7 +48,7 @@ export const BOARD_ALERT_MESSAGE = {
 export const BOARD_ERROR_MESSAGE = {
   CANNOT_GET_POST_LIST_FROM_SERVER: '서버로부터 게시글 목록을 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
   CANNOT_GET_POST_DATA_FROM_SERVER: '서버로부터 게시글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
-  CANNOT_GET_COMMENT_LIST: '서버로부터 댓글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_GET_COMMENT_LIST: '서버로부터 댓글 목록을 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
   CANNOT_CREATE_POST: '게시글 작성에 실패하였습니다. 잠시 후 다시 시도해주세요.',
   NEED_SIGN_IN: '로그인이 필요합니다.',
   ONLY_WRITER_CAN_MODIFY_OR_DELETE_POST: '게시글 수정 및 삭제는 작성자만 가능합니다.',
