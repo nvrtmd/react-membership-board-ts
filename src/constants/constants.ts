9af6150ebb50f7c7c26cf73b35461a2bf62bcfb9
YUZAMIN <untmd.crmn@gmail.com>
2023-02-19 16:05:11 +0900

    feat: 프로젝트에서 사용되는 alert message를 constants.ts 파일 내 정의
    - const 객체에 리터럴 방식으로 할당
    Fixes: #84

---
+++ src/constants/constants.ts
@@ -0,0 +1,31 @@
+export const MEMBER_ALERT_MESSAGE = {
+  NOT_SIGNED_IN_USER_ALERT: '비로그인 사용자입니다.',
+  MEMBER_INFO_MODIFIED_ALERT: '회원 정보가 수정되었습니다.',
+  CHECK_YOUR_INPUT_ALERT: '입력하신 내용이 올바른지 확인해주세요.',
+  DELETE_ACCOUNT_CONFIRM: '정말로 탈퇴하시겠습니까?',
+};
+
+export const AUTH_ALERT_MESSAGE = {
+  ID_OR_PASSWORD_EMPTY_ALERT: '아이디 또는 비밀번호를 입력하세요.',
+};
+
+export const VALIDATION_ALERT_MESSAGE = {
+  ID_VALIDATION_ALERT: '영문 4 ~ 12자를 입력하세요.',
+  PASSWORD_VALIDATION_ALERT: '숫자/특수문자 포함 8자 이상 입력하세요.',
+  NICKNAME_VALIDATION_ALERT: '영어/숫자/한글 4~12자를 입력하세요.',
+};
+
+export const BOARD_ALERT_MESSAGE = {
+  POST_NOT_EXIST_ALERT: '게시글이 존재하지 않습니다.',
+  POST_DELETE_CONFIRM: '게시글을 삭제하시겠습니까?',
+  POST_DELETED_ALERT: '게시글이 삭제되었습니다.',
+  CANNOT_GET_POST_INFO_FROM_SERVER_ALERT: '서버로부터 게시글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CONTENTS_EMPTY_ALERT: '내용을 작성하세요.',
+  TITLE_OR_CONTENTS_EMPTY_ALERT: '제목 또는 본문을 작성하세요.',
+  POST_MODIFIED_ALERT: '게시글이 수정되었습니다.',
+  POST_MODIFIED_CANCEL_CONFIRM: '게시글 수정을 취소하시겠습니까?',
+  NEED_SIGN_IN_CONFIRM: '로그인이 필요합니다. 확인 버튼을 클릭하면 로그인 페이지로 이동합니다.',
+  POST_CREATED_ALERT: '게시글이 작성되었습니다.',
+  POST_CREATE_CANCEL_CONFIRM: '게시글 작성을 취소하시겠습니까?',
+  LOADING_TEXT: 'Loading...',
+};
