e3712c5f9c619ca23c72a276049df46a5c3437a9
YUZAMIN <untmd.crmn@gmail.com>
2023-03-03 20:19:03 +0900

    feat: signUp 에러 핸들링 수정
    - status code에 따라 다른 에러 메세지를 담아서 throw
    Fixes: #95

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { member } from 'apis/member';
 import { AUTH_ERROR_MESSAGE } from 'constants/constants';
 import { Member } from 'global/types';
@@ -7,11 +7,16 @@ const auth = {
   signUp: async (data: Member) => {
     try {
       await axios.post(`/auth/signup`, data);
-    } catch {
-      throw {
-        code: 500,
-        message: AUTH_ERROR_MESSAGE.DUPLICATED_ID,
-      };
+    } catch (err) {
+      const error = err as AxiosError;
+      switch (error.response?.status) {
+        case 409:
+          throw { code: 409, message: AUTH_ERROR_MESSAGE.DUPLICATED_ID };
+        case 500:
+          throw { code: 500, message: AUTH_ERROR_MESSAGE.CANNOT_SIGN_UP };
+        default:
+          throw { code: 500, message: AUTH_ERROR_MESSAGE.CANNOT_SIGN_UP };
+      }
     }
   },
   signIn: async (data: Member) => {
