98b58f10c615b3e576d848569a361f6f8ccfecd4
YUZAMIN <untmd.crmn@gmail.com>
2023-03-20 19:23:37 +0900

    feat: Layout 컴포넌트 내 children을 Outlet으로 대체
    - Router 컴포넌트에서 element가 Layout인 Route로 모든 페이지 Route를
      wrapping
    Fixes: #101

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from 'pages/etc/HomePage';
 import { AboutPage } from 'pages/etc/AboutPage';
 import { ListPage as PostListPage } from 'pages/board/ListPage';
@@ -10,22 +10,25 @@ import { SignInPage } from 'pages/auth/SignInPage';
 import { PrivateRoutes } from 'router/PrivateRoutes';
 import { MyPostsPage } from 'pages/member/MyPostsPage';
 import { MyInfoPage } from 'pages/member/MyInfoPage';
+import { Layout } from 'components/layouts/Layout';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/board/list" element={<PostListPage />} />
-        <Route path="/board/:postIdx" element={<PostPage />} />
-        <Route path="/board/create" element={<CreatePage />} />
-        <Route path="/board/modify/:postIdx" element={<ModifyPage />} />
-        <Route path="/auth/signup" element={<SignUpPage />} />
-        <Route path="/auth/signin" element={<SignInPage />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/member/posts" element={<MyPostsPage />} />
-          <Route path="/member/info" element={<MyInfoPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/board/list" element={<PostListPage />} />
+          <Route path="/board/:postIdx" element={<PostPage />} />
+          <Route path="/board/create" element={<CreatePage />} />
+          <Route path="/board/modify/:postIdx" element={<ModifyPage />} />
+          <Route path="/auth/signup" element={<SignUpPage />} />
+          <Route path="/auth/signin" element={<SignInPage />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/member/posts" element={<MyPostsPage />} />
+            <Route path="/member/info" element={<MyInfoPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
