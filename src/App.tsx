24c79fad5d8891694090bf3d3e830afdb87d0e63
YUZAMIN <untmd.crmn@gmail.com>
2023-03-20 19:17:28 +0900

    feat: App.tsx에 정의한 라우팅 정보를 Router 컴포넌트로 대체
    - index.tsx에서 import한 GlobalStyle을 제거하고 App.tsx에서 import함
    Fixes: #101

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from 'pages/etc/HomePage';
-import { AboutPage } from 'pages/etc/AboutPage';
-import { ListPage as PostListPage } from 'pages/board/ListPage';
-import { PostPage } from 'pages/board/PostPage';
-import { CreatePage } from 'pages/board/CreatePage';
-import { ModifyPage } from 'pages/board/ModifyPage';
-import { SignUpPage } from 'pages/auth/SignUpPage';
-import { SignInPage } from 'pages/auth/SignInPage';
-import { PrivateRoutes } from 'router/PrivateRoutes';
-import { MyPostsPage } from 'pages/member/MyPostsPage';
-import { MyInfoPage } from 'pages/member/MyInfoPage';
+import GlobalStyle from 'styles/GlobalStyle';
+import { Router } from 'router/Router';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/board/list" element={<PostListPage />} />
-        <Route path="/board/:postIdx" element={<PostPage />} />
-        <Route path="/board/create" element={<CreatePage />} />
-        <Route path="/board/modify/:postIdx" element={<ModifyPage />} />
-        <Route path="/auth/signup" element={<SignUpPage />} />
-        <Route path="/auth/signin" element={<SignInPage />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/member/posts" element={<MyPostsPage />} />
-          <Route path="/member/info" element={<MyInfoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import App from 'App';
-import GlobalStyle from 'styles/GlobalStyle';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>,
-);
+root.render(<App />);
